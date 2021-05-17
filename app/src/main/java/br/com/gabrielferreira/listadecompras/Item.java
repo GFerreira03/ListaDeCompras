28d35ea8540cee610c72454203957d55167601fb
Gabriel Ferreira <43641763+GFerreira03@users.noreply.github.com>
2021-05-17 10:36:25 -0300

    Add item to BD

--- app/src/main/java/br/com/gabrielferreira/listadecompras/Item.java
+++ app/src/main/java/br/com/gabrielferreira/listadecompras/Item.java
@@ -2,10 +2,10 @@ package br.com.gabrielferreira.listadecompras;
 
 public class Item {
     public final int quantity;
-    public final int name;
+    public final String name;
 
 
-    public Item(int quantity, int name) {
+    public Item(int quantity, String name) {
         this.quantity = quantity;
         this.name = name;
     }
--- app/src/main/java/br/com/gabrielferreira/listadecompras/newItem_dialog.java
+++ app/src/main/java/br/com/gabrielferreira/listadecompras/newItem_dialog.java
@@ -1,9 +1,11 @@
 package br.com.gabrielferreira.listadecompras;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
@@ -15,6 +17,9 @@ import androidx.appcompat.app.AppCompatDialogFragment;
 public class newItem_dialog extends AppCompatDialogFragment {
     private EditText quantityTxt;
     private EditText nameTxt;
+
+    public int quantity;
+    public String name;
 
     @NonNull
     @Override
@@ -39,7 +44,10 @@ public class newItem_dialog extends AppCompatDialogFragment {
                 .setPositiveButton("Adicionar", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
-
+                        Item item = new Item(quantity, name);
+                        ShoppingDb db = ShoppingDb.getInstance(getContext());
+                        db.addItem(item);
+                        Log.v("Add", "Item adicionado");
                     }
                 });
 
